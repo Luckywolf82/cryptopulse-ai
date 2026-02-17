568091c21a4b146b2b58cbd67b54329fee057328
trygve waagen <trygve.waagen@gmail.com>
2026-02-17 13:42:32 +0000

    File changes

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button";
 import { Globe } from "lucide-react";
+import "@/components/lib/i18n";
 
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation();
